139b6ef3900b157e63d884a34b5624f403956a17
xiaotian <915155536@qq.com>
2022-10-28 16:57:35 +0800

    测试

---
+++ test/demo1.py
@@ -0,0 +1,17 @@
+#  ===========================
+# -*- coding:utf-8 -*-
+# Time :2022/10/28 16:41
+# Author :小灬天
+# QQ:915155536
+# File :demo1.py
+#  ===========================
+import unittest
+
+print('hello!!!')
+
+
+class Test(unittest.TestCase):
+    def test_01(self):
+        assert 1 > 2
+
+
